f2cc0a4fd3f37da05a6ed8ba181bcfc26a5d5b9f
penafiel.erick.martin <penafiel.erick.martin@gmail.com>
2025-11-23 16:14:52 -0400

    Add authentication

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,67 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { createClient } from "@/lib/supabase/client";
+import { redirect } from "next/navigation";
+import { JSX } from "react";
+
+async function UserDetails() {
+	const supabase = await createClient();
+	const { data, error } = await supabase.auth.getClaims();
+
+	if (error || !data?.claims) {
+		redirect("/auth/login");
+	}
+
+	return JSON.stringify(data.claims, null, 2);
+}
+
+export default function DashboardLayout({
+	children,
+}: {
+	children: JSX.Element;
+}) {
+	return (
+		<SidebarProvider>
+			<UserDetails />
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+					<div className="flex items-center gap-2 px-4">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							orientation="vertical"
+							className="mr-2 data-[orientation=vertical]:h-4"
+						/>
+						<Breadcrumb>
+							<BreadcrumbList>
+								<BreadcrumbItem className="hidden md:block">
+									<BreadcrumbLink href="#">
+										Building Your Application
+									</BreadcrumbLink>
+								</BreadcrumbItem>
+								<BreadcrumbSeparator className="hidden md:block" />
+								<BreadcrumbItem>
+									<BreadcrumbPage>Data Fetching</BreadcrumbPage>
+								</BreadcrumbItem>
+							</BreadcrumbList>
+						</Breadcrumb>
+					</div>
+				</header>
+				<div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+			</SidebarInset>
+		</SidebarProvider>
+	);
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,55 +1,43 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  )
+import { createClient } from "@/lib/supabase/server";
+import { InfoIcon } from "lucide-react";
+import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
+import { Suspense } from "react";
+
+async function UserDetails() {
+	const supabase = await createClient();
+	const { data, error } = await supabase.auth.getClaims();
+
+	if (error || !data?.claims) {
+		redirect("/auth/login");
+	}
+
+	return JSON.stringify(data.claims, null, 2);
+}
+
+export default function ProtectedPage() {
+	return (
+		<div className="flex-1 w-full flex flex-col gap-12">
+			<div className="w-full">
+				<div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
+					<InfoIcon size="16" strokeWidth={2} />
+					This is a protected page that you can only see as an authenticated
+					user
+				</div>
+			</div>
+			<div className="flex flex-col gap-2 items-start">
+				<h2 className="font-bold text-2xl mb-4">Your user details</h2>
+				<pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
+					<Suspense>
+						<UserDetails />
+					</Suspense>
+				</pre>
+			</div>
+			<div>
+				<h2 className="font-bold text-2xl mb-4">Next steps</h2>
+				<FetchDataSteps />
+			</div>
+		</div>
+	);
 }
