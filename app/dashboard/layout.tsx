6ebc54e5de4e93b7affd2afb8f14f82685f688dc
Peñafiel Erick <penafiel.erick.martin@gmail.com>
2025-11-25 20:00:39 -0400

    Add calendar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -35,7 +35,7 @@ export default function DashboardLayout({
 }) {
 	return (
 		<SidebarProvider>
-			<UserDetails />
+			{/* <UserDetails /> */}
 			<AppSidebar />
 			<SidebarInset>
 				<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
